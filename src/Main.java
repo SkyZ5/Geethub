4298b5ec6a637bfcaee7056097a379a8d5f84cbe
BT_2S14_15 <kmei0013@yahoo.com>
2024-09-16 10:11:03 -0400

    A added one additional unique distinct print statement that describes the specific food in which I like/prefer to eat.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.print("Kevin Mei");
+
+        System.out.println("Pineapple is a very good food, but I'm allergic to it");
     }
 }
